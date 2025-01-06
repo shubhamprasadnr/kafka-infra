5ee91d95fafdd9c71cf811b788c3a6cdcefeb412
YugeshKumar01 <64956995+YugeshKumar01@users.noreply.github.com>
2025-01-07 00:03:11 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,11 +3,11 @@ provider "aws" {
   region = "us-east-1"  # Set the region for AWS resources
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "s3-bucket-batch28-yugesh"  # Hardcode the bucket name here
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#     encrypt = true
-#   }
-# }
+ terraform {
+   backend "s3" {
+     bucket = "s3-bucket-batch28-yugesh"  # Hardcode the bucket name here
+     key    = "terraform.tfstate"
+     region = "us-east-1"
+     encrypt = true
+   }
+ }
